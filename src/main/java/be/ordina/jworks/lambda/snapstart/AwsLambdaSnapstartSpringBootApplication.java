319d207d5ecc5a2ae01faebc03c8f9b344af9053
Yolan Vloeberghs <yolanvloeberghs@gmail.com>
2022-12-23 10:24:56 +0100

    Initial commit

---
+++ src/main/java/be/ordina/jworks/lambda/snapstart/AwsLambdaSnapstartSpringBootApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.jworks.lambda.snapstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaSnapstartSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsLambdaSnapstartSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ordina/jworks/lambda/snapstart/AwsLambdaSnapstartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ordina.jworks.lambda.snapstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaSnapstartSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
